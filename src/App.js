861cf4df968d83461db179f8e5ecf2925488daf6
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-03 21:54:08 +0530

    added page layout + routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Box } from "@mui/material";
 
 import "./App.css";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 };
 
