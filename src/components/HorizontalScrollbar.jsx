9facfc5802adbb93651cbaa549869018ff5d5cf4
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-08-29 17:36:32 +0530

    fix: browser console warnings

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -38,7 +38,7 @@ const HorizontalScrollbar = ({ data, isBodyParts, setBodyPart, bodyPart }) => (
     {data.map((item) => (
       <Box
         key={item?.id || item}
-        itemId={item?.id || item}
+        itemID={item?.id || item}
         title={item?.id || item}
         m="0 40px"
       >
